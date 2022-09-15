28ffa787930e04b082f646742f61bffeac6058ed
Pisco <ajpisco_pro@gmail.com>
2022-09-15 21:11:26 +0100

    Review comments

--- modules/lb/data.tf
+++ modules/lb/data.tf
@@ -1,9 +1,15 @@
 data "google_project" "current_project" {
 }
 
-data "google_compute_subnetwork" "default" {
-  count = can(var.subnetwork) ? 0 : 1
-  name   = "default"
+data "google_compute_subnetwork" "public_subnet" {
+  count = (can(var.subnetwork) || (var.scheme != "EXTERNAL_MANAGED" && var.scheme != "EXTERNAL")) ? 0 : 1
+  name   = "${data.google_project.current_project.project_id}-public-subnet"
+  region = var.region
+}
+
+data "google_compute_subnetwork" "private_subnet" {
+  count = (can(var.subnetwork) || var.scheme != "INTERNAL_SELF_MANAGED") ? 0 : 1
+  name   = "${data.google_project.current_project.project_id}-private-subnet"
   region = var.region
 }
 
